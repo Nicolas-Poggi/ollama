3391c59539abad123865a1245fb9cd80afe7bc30
Jesse Gross <jesse@ollama.com>
2025-03-20 10:35:19 -0700

    ollamarunner: Provide mechanism for backends to loading progress
    This enables the runner to report progress back to the Ollama server,
    both for showing status to the user and also to prevent the server
    from killing the runner if it thinks things have stalled.
    
    Most of the infrastructure was already there, this extends it to
    be available to the backends.

--- runner/ollamarunner/runner.go
+++ runner/ollamarunner/runner.go
@@ -782,6 +782,9 @@ func Execute(args []string) error {
 	}
 
 	params := ml.BackendParams{
+		Progress: func(progress float32) {
+			server.progress = progress
+		},
 		NumThreads:     *threads,
 		NumGPULayers:   *numGPULayers,
 		MainGPU:        *mainGPU,
